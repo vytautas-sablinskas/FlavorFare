b51824ec03d779ecde1a9a6a70c5dffce420d90d
vytcka <vytas.sa1@gmail.com>
2023-12-10 22:48:25 +0200

    Retry of cors

--- FlavorFare/FlavorFare.API/Program.cs
+++ FlavorFare/FlavorFare.API/Program.cs
@@ -105,7 +105,7 @@ namespace FlavorFare.API
                 options.AddPolicy("AllowReactApp",
                     builder =>
                     {
-                        builder.WithOrigins("https://flavor-fare.vercel.app/")
+                        builder.WithOrigins("https://flavor-fare.vercel.app")
                                .AllowAnyHeader()
                                .AllowAnyMethod()
                                .AllowCredentials();
